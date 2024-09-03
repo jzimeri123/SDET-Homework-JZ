1f22cdfb42ac544e393f328517468e5732c64a63
jzimeri123 <jzimeri@gmail.com>
2024-09-03 19:02:50 -0400

    Commiting to update

--- src/main/java/org/example/e144/E144Constructor.java
+++ src/main/java/org/example/e144/E144Constructor.java
@@ -1,9 +1,9 @@
 package org.example.e144;
 public class E144Constructor {
     public static void main(String[] args) {
-        StoreProduct obj1=new StoreProduct("Eggs",3,"Produce",true,10);
-        StoreProduct obj2=new StoreProduct("Paper Towels",2,24);
-        StoreProduct obj3=new StoreProduct("Paper Towels",2);
+        StoreProduct obj1=new StoreProduct("Eggs",3.0,"Produce",true,10);
+        StoreProduct obj2=new StoreProduct("Paper Towels",2.0,24);
+        StoreProduct obj3=new StoreProduct("Paper Towels",2.0);
         obj1.display();
         obj2.display();
         obj3.display();
@@ -19,7 +19,7 @@ class StoreProduct{
     private int stock;
 
     //main constructor
-    StoreProduct(String label,double price,String category,boolean hasExpiration,int stock){
+    public StoreProduct(String label,double price,String category,boolean hasExpiration,int stock){
         this.label=label;
         this.price=price;
         this.category=category;
@@ -27,7 +27,7 @@ class StoreProduct{
         this.stock=stock;
     }
     //second constructor
-    StoreProduct(String label,double price,int stock){
+    public StoreProduct(String label,double price,int stock){
         this.label=label;
         this.price=price;
         this.category="misc";
@@ -35,10 +35,10 @@ class StoreProduct{
         this.stock=stock;
     }
     //third constructor
-    StoreProduct(String label,double price){
+    public StoreProduct(String label,double price){
         this.label=label;
         this.price=price;
-        this.category=category;
+        this.category="misc";
         this.hasExpiration=false;
         this.stock=0;
     }
